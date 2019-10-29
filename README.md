ecd0b7b6bc56b219edd30bc095e9dc11f4c4d1eb
Tony-Moon <anthonyowenmoon@gmail.com>
2019-10-29 00:31:01 -0500

    cannot seem to load in sprite when handler is built

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Project-1
 Expiramenting with networking and remote machines.
 Project 1 will be an instant messager using Go hosted on an Ubuntu machine on the AWS cloud.
+
+
+Remember to run:
+rice embed-go
--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 )
 
 func main() {
+	// router := mux.NewRouter()
+	// router.PathPrefix("/").Handler(http.FileServer(rice.MustFindBox("web/template").HTTPBox()))
+
+	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	http.HandleFunc("/home", web.Home)
 	http.HandleFunc("/chat", web.Chat)
 	http.HandleFunc("/ttt", web.T3)
 	http.HandleFunc("/alog", web.AdminLogin)
 	http.HandleFunc("/admin", web.Admin)
+
 	http.ListenAndServe(":9000", nil)
 }
