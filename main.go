0bb54d813b9bf7583a9ff4f5817b515a9ff1c974
Tony-Moon <anthonyowenmoon@gmail.com>
2019-11-01 09:58:51 -0500

    Trying a different approach

--- main.go
+++ main.go
@@ -14,5 +14,5 @@ func main() {
 	http.HandleFunc("/ulog", chat.UserLogin)
 	http.HandleFunc("/buffer", chat.Buffer)
 	http.HandleFunc("/rebuffer", chat.ReBuffer)
-	http.ListenAndServe("ec2-3-19-77-195.us-east-2.compute.amazonaws.com:80", nil)
+	http.ListenAndServe(":9000", nil)
 }
