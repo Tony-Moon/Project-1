601e295a51d7ad05dbee2b926ee905a1e2f39ebb
Tony-Moon <anthonyowenmoon@gmail.com>
2019-11-04 11:14:03 -0600

    Using diefferent IP

--- chat/Buffer.go
+++ chat/Buffer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
@@ -22,6 +23,7 @@ func Buffer(res http.ResponseWriter, req *http.Request) {
 	nickname = req.FormValue("name")
 
 	ip := GetIP()
+	fmt.Println(ip)
 	NickIPs[ip] = nickname
 	fmt.Println(NickIPs[ip], "has joined the chat room!")
 
@@ -45,19 +47,8 @@ func ReBuffer(res http.ResponseWriter, req *http.Request) {
 
 // GetIP doc
 func GetIP() string {
-	ifaces, _ := net.Interfaces()
-	var ip net.IP
-	for _, i := range ifaces {
-		addrs, _ := i.Addrs()
-		for _, addr := range addrs {
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-		}
-	}
-	ips := ip.String()
-	return ips
+	conn, _ := net.Dial("udp", "8.8.8.8:80")
+	localAddr := conn.LocalAddr().String()
+	idx := strings.LastIndex(localAddr, ":")
+	return localAddr[0:idx]
 }
